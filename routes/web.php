6a0d41893dd52f8f1d16bf7f87370b6c80dde464
88Er <zhm898885242@sina.cn>
2018-09-28 18:35:45 +0800

    第三次

--- routes/web.php
+++ routes/web.php
@@ -26,16 +26,12 @@ Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
 // Password Reset Routes...
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
-)->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
-)->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
-')->name('password.reset');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
-]]);
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 
 Route::resource('projects', 'ProjectsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
@@ -47,5 +43,4 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
-Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
-);
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
