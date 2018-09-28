037898fb0fa4055da081e96d1cbefe4d4bd1cb28
88Er <zhm898885242@sina.cn>
2018-09-28 17:11:39 +0800

    小改

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -13,7 +13,8 @@
                     <div class="media">
                         <div align="center">
                             <a href="{{ route('users.show', $topic->user->id) }}">
-                                <img class="thumbnail img-responsive" src="{{ $topic->user->avatar }}" width="300px" height="300px">
+                                <img class="thumbnail img-responsive" src="{{ $topic->user->avatar }}" width="300px"
+                                     height="300px">
                             </a>
                         </div>
                     </div>
@@ -34,15 +35,25 @@
                     </div>
                     <div class="topic-body">
                         {!! $topic->body !!}
-                    </div><div class="operate">
+                    </div>
+                    <div class="operate">
                         <hr>
                         <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-default btn-xs" role="button">
                             <i class="glyphicon glyphicon-edit"></i> 编辑
                         </a>
-                        <a href="{{ route('topics.destroy', $topic->id) }}" class="btn btn-default btn-xs" role="button">
+                        <a href="{{ route('topics.destroy', $topic->id) }}" class="btn btn-default btn-xs"
+                           role="button">
                             <i class="glyphicon glyphicon-trash"></i> 删除
                         </a>
                     </div>
+                </div>
+            </div>
+            {{-- 用户回复列表 --}}
+            <div class="panel panel-default topic-reply">
+                <div class="panel-body">
+                    @includeWhen(Auth::check(), 'topics._reply_box', ['topic' => $topic])
+                    @include('topics._reply_list', ['replies' => $topic->replies()->
+with('user')->get()])
                 </div>
             </div>
         </div>
